4c4529a6e34594fa5e1eb3641922e56f33e3c1b8
QuinlanVic <Quinlanvc@gmail.com>
2024-03-23 13:51:08 +0200

    policiespage, about and test apis

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request, render_template
+import json
 
 # Create a instance of Flask
-app = Flask(__name__) 
+app = Flask(__name__)
 
 # some dummy employee data
 employees = [
@@ -10,61 +11,68 @@ employees = [
         "name": "Paul Flex",
         "job-title": "CEO",
         "pic": "https://t4.ftcdn.net/jpg/06/35/15/47/360_F_635154757_zU5ZxxZe3Vs0hrrOQ9WBgNgX8s4Cw19s.jpg",
-        "desc": "Paul Flex is CoolWater's Chief Executive Officer"
+        "desc": "Paul Flex is CoolWater's Chief Executive Officer",
     },
     {
         "id": 2,
         "name": "Sarah Blue",
         "job-title": "CFO",
         "pic": "https://t4.ftcdn.net/jpg/06/12/73/89/360_F_612738927_LIcFCiKHQhHq9R1QhkVRKvT6RelYUmgv.jpg",
-        "desc": "Sarah Bleu is CoolWater's Chief Financial Officer"
+        "desc": "Sarah Bleu is CoolWater's Chief Financial Officer",
     },
     {
         "id": 3,
         "name": "Mandla Ngcobo",
         "job-title": "CHRO",
         "pic": "https://www.goodthingsguy.com/wp-content/uploads/2020/07/vusi-thembekwayo-biography-age-net-worth-wife-2.jpg",
-        "desc": "Mandla Ngcobo is Coolwater's Chief Human Resources Officer"
-    }
+        "desc": "Mandla Ngcobo is Coolwater's Chief Human Resources Officer",
+    },
 ]
 
 # some dummy policy data
 policies = [
     {
         "id": 1,
-        "name": "Cool Car Insurance",
-        "price": 1000,
-        "poster": "",
-        "desc": ""
+        "name": "Blue Car Insurance",
+        "price": 500,
+        "poster": "https://c4.wallpaperflare.com/wallpaper/14/17/129/2011-hyundai-i10-wallpaper-preview.jpg",
+        "desc": f"Blue Car Insurance if affordable and is targeted at students and young professionals",
     },
     {
         "id": 2,
-        "name": "Water Car Insurance",
-        "price": 1500,
-        "poster": "",
-        "desc": ""
+        "name": "Cool Car Insurance",
+        "price": 1000,
+        "poster": "https://c4.wallpaperflare.com/wallpaper/373/463/605/volkswagen-rain-gti-hd-silver-coupe-wallpaper-preview.jpg",
+        "desc": "Cool Car Insurance is targeted at established professionals and middle aged individuals",
     },
     {
         "id": 3,
-        "name": "CoolWater Car Insurance",
-        "price": 2000,
-        "poster": "",
-        "desc": ""
+        "name": "Water Car Insurance",
+        "price": 1500,
+        "poster": "https://moewalls.com/wp-content/uploads/2024/01/bmw-m4-rainy-night-thumb.jpg",
+        "desc": "Water Car Insurance is targeted at small families",
     },
     {
         "id": 4,
-        "name": "CoolCool Car Insurance",
-        "price": 2500,
-        "poster": "",
-        "desc": ""
+        "name": "CoolWater Car Insurance",
+        "price": 2000,
+        "poster": "https://c4.wallpaperflare.com/wallpaper/634/597/281/luxury-suv-range-rover-velar-2017-4k-wallpaper-preview.jpg",
+        "desc": "CoolWater is targeted at medium to large families",
     },
     {
         "id": 5,
+        "name": "CoolCool Car Insurance",
+        "price": 2500,
+        "poster": "https://c4.wallpaperflare.com/wallpaper/131/207/698/forest-rain-forest-ford-wallpaper-preview.jpg",
+        "desc": "CoolCool Car Insurance is targeted at those with antique vehicles",
+    },
+    {
+        "id": 6,
         "name": "WaterCool Car Insurance",
         "price": 3000,
-        "poster": "",
-        "desc": ""
-    }
+        "poster": "https://i.pinimg.com/originals/56/c3/74/56c3744dc273cab7d050b9943d2accfa.jpg",
+        "desc": "WaterCool Car Insurance is targeted at those with sports cars",
+    },
 ]
 
 # some dummy user data
@@ -74,52 +82,83 @@ users = [
         "name": "Ethan Rich",
         "email": "E.rich@gmail.com",
         "pic": "",
+        "policy-id": 0,
     },
     {
         "id": 2,
         "name": "Scarlet Ndlovu",
         "email": "Scar.let@gmail.com",
         "pic": "",
+        "policy-id": 1,
     },
     {
         "id": 3,
         "name": "Bertha Pudding",
         "email": "Bertha@gmail.com",
         "pic": "",
-    }
+        "policy-id": 4,
+    },
 ]
 
-# root URL 
+
+# root URL
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
+
 # policies page
 @app.route("/policies")
 def policies_page():
-    return render_template("policies.html", policies=policies) 
+    # policies = json.loads(get_policies())
+    return render_template("policies.html", policies=policies)
 
-# policy page 
+
+# policy page
 @app.route("/policies/<id>")
 def policy_page(id):
-    return render_template("policy.html")
+    policy = get_policy(id)
+    print(policy)
+    return render_template("policy.html", policy=policy["data"])
+
 
 # Define a route for the /about URL
 @app.route("/about")
 def about_page():
     return render_template("about.html", employees=employees)
 
+
 # Define a route for the /help page
 @app.route("/help")
 def help_page():
     return render_template("help.html")
 
+
 # Define a route for the /profile page
-@app.route("/profile/<id>") 
+@app.route("/profile/<id>")
 def profile_page(id):
-    return render_template("profile.html")
+    profile = get_user(id)
+    return render_template("profile.html", profile=profile)
+
+
+# Define a route for the login page
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup_page():
+    return render_template("signup.html")
+
 
 # ************ CRUD OPERATIONS FOR USER ************
+# GET -> all users -> JSON
+@app.get("/users")
+def get_users():
+    # have to convert to JSON (using jsonify library from Flask)
+    return jsonify(users)
+
 
 # GET -> specific user -> JSON
 @app.get("/users/<id>")
@@ -135,36 +174,38 @@ def get_user(id):
     result = {"message": "user successfully found", "data": specific_user}
     return jsonify(result)
 
+
 # DELETE -> specific user -> JSON
 @app.delete("/users/<id>")
 def delete_user(id):
     # search through users list and find the user with the specified id (None (default value) if not found)
     specific_user = next((user for user in users if user["id"] == int(id)), None)
-     # if user is not found
+    # if user is not found
     if specific_user is None:
         result = {"message": "user not found"}
         # return a not found message and appropriate status code (json)
         return jsonify(result), 404
-    
+
     # otherwise user has been found so we return a success message and the user data (json)
     # delete user from users list
     users.remove(specific_user)
     result = {"message": "user successfully deleted", "data": specific_user}
     return jsonify(result)
 
-# PUT -> request.json -> update specific user -> JSON 
+
+# PUT -> request.json -> update specific user -> JSON
 @app.put("/users/<id>")
 def update_user(id):
     # get update data from PUT request body
     update_data = request.json
     # search through users list and find the user with the specified id (None (default value) if not found)
     specific_user = next((user for user in users if user["id"] == int(id)), None)
-     # if user is not found
+    # if user is not found
     if specific_user is None:
         result = {"message": "user not found"}
         # return a not found message and appropriate status code (json)
         return jsonify(result), 404
-    
+
     # otherwise user has been found so we return a success message and user data (json)
     # update all values in "specific_user" with values from "update_data" dictionary
     # it changes in place due to us accessing values via memory address ("next" function)
@@ -175,6 +216,7 @@ def update_user(id):
 
     result = {"message": "user has been successfully updated", "data": specific_user}
     return jsonify(result)
+
 
 # POST -> request.json -> create user and add to users list -> json
 @app.post("/users")
@@ -194,12 +236,22 @@ def create_user():
     # added appropriate status code
     return jsonify(result), 201
 
+
 # ************ CRUD OPERATIONS FOR POLICIES ************
+# GET -> all policies -> JSON
+@app.get("/policies-data")
+def get_policies():
+    # have to convert to JSON (using jsonify library from Flask)
+    return jsonify(policies)
+
+
 # GET -> specific policy -> JSON
-@app.get("/policies/<id>")
+@app.get("/policiesdata/<id>")
 def get_policy(id):
     # search through policies list and find the policy with the specified id (None (default value) if not found)
-    specific_policy = next((policy for policy in policies if policy["id"] == int(id)), None)
+    specific_policy = next(
+        (policy for policy in policies if policy["id"] == int(id)), None
+    )
     # if policy is not found
     if specific_policy is None:
         result = {"message": "policy not found"}
@@ -209,36 +261,42 @@ def get_policy(id):
     result = {"message": "policy successfully found", "data": specific_policy}
     return jsonify(result)
 
+
 # DELETE -> specific policy -> JSON
-@app.delete("/policies/<id>")
+@app.delete("/policiesdata/<id>")
 def delete_policy(id):
     # search through policies list and find the policy with the specified id (None (default value) if not found)
-    specific_policy = next((policy for policy in policies if policy["id"] == int(id)), None)
-     # if policy is not found
+    specific_policy = next(
+        (policy for policy in policies if policy["id"] == int(id)), None
+    )
+    # if policy is not found
     if specific_policy is None:
         result = {"message": "policy not found"}
         # return a not found message and appropriate status code (json)
         return jsonify(result), 404
-    
+
     # otherwise policy has been found so we return a success message and the policy data (json)
     # delete policy from policies list
     policies.remove(specific_policy)
     result = {"message": "policy successfully deleted", "data": specific_policy}
     return jsonify(result)
 
-# PUT -> request.json -> update specific policy -> JSON 
-@app.put("/policies/<id>")
+
+# PUT -> request.json -> update specific policy -> JSON
+@app.put("/policiesdata/<id>")
 def update_policy(id):
     # get update data from PUT request body
     update_data = request.json
     # search through policies list and find the policy with the specified id (None (default value) if not found)
-    specific_policy = next((policy for policy in policies if policy["id"] == int(id)), None)
-     # if policy is not found
+    specific_policy = next(
+        (policy for policy in policies if policy["id"] == int(id)), None
+    )
+    # if policy is not found
     if specific_policy is None:
         result = {"message": "policy not found"}
         # return a not found message and appropriate status code (json)
         return jsonify(result), 404
-    
+
     # otherwise policy has been found so we return a success message and policy data (json)
     # update all values in "specific_policy" with values from "update_data" dictionary
     # it changes in place due to us accessing values via memory address ("next" function)
@@ -247,11 +305,15 @@ def update_policy(id):
     # print(specific_policy)
     # print(policies)
 
-    result = {"message": "policy has been successfully updated", "data": specific_policy}
+    result = {
+        "message": "policy has been successfully updated",
+        "data": specific_policy,
+    }
     return jsonify(result)
 
+
 # POST -> request.json -> create policy and add to policies list -> json
-@app.post("/policies")
+@app.post("/policiesdata")
 def create_policy():
     new_policy = request.json
     policy_ids = [policy["id"] for policy in policies]
@@ -267,14 +329,3 @@ def create_policy():
     result = {"message": "policy successfully added", "data": new_policy}
     # added appropriate status code
     return jsonify(result), 201
-
-# Define a route for the login page
-@app.route("/login")
-def login_page():
-    return render_template("login.html")
-
-@app.route("/signup")
-def signup_page():
-    return render_template("signup.html")
-
-
