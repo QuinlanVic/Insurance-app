5ba8fa698e29a92d7d81c2cca4d4dcaedb1bda35
QuinlanVic <Quinlanvc@gmail.com>
2024-03-25 21:59:42 +0200

    header and login and sign up

--- app.py
+++ app.py
@@ -114,6 +114,7 @@ def policies_page():
     return render_template("policies.html", policies=policies)
 
 
+# one policy entry to test what policy page displays
 policylocal = {
     "id": 1,
     "name": "Blue Car Insurance",
@@ -159,6 +160,51 @@ def login_page():
 @app.route("/signup")
 def signup_page():
     return render_template("signup.html")
+
+
+# go to homepage when posting from login page
+@app.route("/", methods=["POST"])
+def go_to_index():
+    # have to get values from form via keys
+    email = request.form.get("email")
+    password = request.form.get("password")
+    print("Profile page", email, password)
+    return "<h1>Welcome back</h1>", render_template("index.html")
+
+
+# go to profile page when posting from signup page
+@app.route("/profile", methods=["POST"])
+def go_to_profile():
+    # have to get values from form via keys
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    # have to create a new user and add them to user list
+    new_user = {
+        "name": name,
+        "email": email,
+        password: "password",
+        "pic": "",
+        "policy-id": 0,
+    }
+
+    user_ids = [user["id"] for user in users]
+    # print(user_ids) # check if you have got all the ids successfully
+    max_id = max(user_ids)
+    # print(max_id) # check if you have max id sucessfully
+    new_user["id"] = max_id + 1
+    # print(new_user) # check if new user has been crafted successfully
+
+    # add to users list of dict
+    users.append(new_user)
+    print("Profile page", email, password)
+    return render_template("profile.html", user=new_user)
+
+
+# testing route for formatting/developing header and footer in "base.html"
+@app.route("/base")
+def base_page():
+    return render_template("base.html")
 
 
 # ************ CRUD OPERATIONS FOR USER ************
