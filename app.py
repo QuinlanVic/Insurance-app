a1225a8d98c73a0a3a19b3cb5fb7f507d7bbc62a
QuinlanVic <Quinlanvc@gmail.com>
2024-03-26 08:29:00 +0200

    uniform bgd color and updates

--- app.py
+++ app.py
@@ -127,9 +127,22 @@ policylocal = {
 # policy page
 @app.route("/policies/<id>")
 def policy_page(id):
-    policy = get_policy(id)
-    print(type(policy))
-    return render_template("policy.html", policy=policylocal)
+    # policy = get_policy(id)
+    # print(type(policy))
+    # search through policies list and find the policy with the specified id (None (default value) if not found)
+    print(id)
+    specific_policy = next(
+        (policy for policy in policies if policy["id"] == int(id)), None
+    )
+    # if policy is not found
+    if specific_policy is None:
+        # result = {"message": "policy not found"}
+        # return a not found message and appropriate status code (json)
+        return "<h1>Error: Policy not found :(</h1>"
+    # otherwise policy has been found so we return a success message and the policy data (json)
+    # result = {"message": "policy successfully found", "data": specific_policy}
+    # return jsonify(result)
+    return render_template("policy.html", policy=specific_policy)
 
 
 # Define a route for the /about URL
