e0d8e375d967c1723a7b4c79bcc586af69d24f4a
QuinlanVic <Quinlanvc@gmail.com>
2024-04-22 00:21:02 +0200

    local data fixed

--- localdata.py
+++ localdata.py
@@ -1,21 +1,18 @@
 # some dummy employee data
 employees = [
     {
-        "id": 1,
         "name": "Paul Flex",
         "job_title": "CEO",
         "pic": "https://t4.ftcdn.net/jpg/06/35/15/47/360_F_635154757_zU5ZxxZe3Vs0hrrOQ9WBgNgX8s4Cw19s.jpg",
         "desc": "Paul Flex is CoolWater's Chief Executive Officer",
     },
     {
-        "id": 2,
         "name": "Sarah Blue",
         "job_title": "CFO",
         "pic": "https://t4.ftcdn.net/jpg/06/12/73/89/360_F_612738927_LIcFCiKHQhHq9R1QhkVRKvT6RelYUmgv.jpg",
         "desc": "Sarah Bleu is CoolWater's Chief Financial Officer",
     },
     {
-        "id": 3,
         "name": "Mandla Ngcobo",
         "job_title": "CHRO",
         "pic": "https://www.goodthingsguy.com/wp-content/uploads/2020/07/vusi-thembekwayo-biography-age-net-worth-wife-2.jpg",
@@ -26,42 +23,36 @@ employees = [
 # some dummy policy data
 policies = [
     {
-        "id": 1,
         "name": "Blue Car Insurance",
         "price": 500,
         "poster": "https://c4.wallpaperflare.com/wallpaper/14/17/129/2011-hyundai-i10-wallpaper-preview.jpg",
         "desc": "Blue Car Insurance is affordable and is targeted at students and young professionals",
     },
     {
-        "id": 2,
         "name": "Cool Car Insurance",
         "price": 1000,
         "poster": "https://c4.wallpaperflare.com/wallpaper/373/463/605/volkswagen-rain-gti-hd-silver-coupe-wallpaper-preview.jpg",
         "desc": "Cool Car Insurance is targeted at established professionals and middle aged individuals",
     },
     {
-        "id": 3,
         "name": "Water Car Insurance",
         "price": 1500,
         "poster": "https://moewalls.com/wp-content/uploads/2024/01/bmw-m4-rainy-night-thumb.jpg",
         "desc": "Water Car Insurance is targeted at small families",
     },
     {
-        "id": 4,
         "name": "CoolWater Car Insurance",
         "price": 2000,
         "poster": "https://c4.wallpaperflare.com/wallpaper/634/597/281/luxury-suv-range-rover-velar-2017-4k-wallpaper-preview.jpg",
         "desc": "CoolWater is targeted at medium to large families",
     },
     {
-        "id": 5,
         "name": "CoolCool Car Insurance",
         "price": 2500,
         "poster": "https://c4.wallpaperflare.com/wallpaper/131/207/698/forest-rain-forest-ford-wallpaper-preview.jpg",
         "desc": "CoolCool Car Insurance is targeted at those with antique vehicles",
     },
     {
-        "id": 6,
         "name": "WaterCool Car Insurance",
         "price": 3000,
         "poster": "https://i.pinimg.com/originals/56/c3/74/56c3744dc273cab7d050b9943d2accfa.jpg",
@@ -70,29 +61,26 @@ policies = [
 ]
 
 # some dummy user data
-users = [
-    {
-        "id": 1,
-        "name": "Ethan Rich",
-        "email": "E.rich@gmail.com",
-        "pic": "",
-        "policy-id": 0,
-    },
-    {
-        "id": 2,
-        "name": "Scarlet Ndlovu",
-        "email": "Scar.let@gmail.com",
-        "pic": "",
-        "policy-id": 1,
-    },
-    {
-        "id": 3,
-        "name": "Bertha Pudding",
-        "email": "Bertha@gmail.com",
-        "pic": "",
-        "policy-id": 4,
-    },
-]
+# users = [
+#     {
+#         "name": "Ethan Rich",
+#         "email": "E.rich@gmail.com",
+#         "pic": "",
+#         "policy-id": 0,
+#     },
+#     {
+#         "name": "Scarlet Ndlovu",
+#         "email": "Scar.let@gmail.com",
+#         "pic": "",
+#         "policy-id": 1,
+#     },
+#     {
+#         "name": "Bertha Pudding",
+#         "email": "Bertha@gmail.com",
+#         "pic": "",
+#         "policy-id": 4,
+#     },
+# ]
 
 # articles dummy data
 # id	title	author	poster	desc
